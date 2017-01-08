da665fe0237537e27f478e7e5f13d26ca65145f2
zxh965321@163.com <zxh965321@163.com>
2017-01-08 19:12:37 +0800

    第一次提交

---
+++ app/src/main/java/com/kodelabs/boilerplate/domain/repository/Repository.java
@@ -0,0 +1,17 @@
+package com.kodelabs.boilerplate.domain.repository;
+
+import com.kodelabs.boilerplate.domain.model.SampleModel;
+
+/**
+ * A sample repository with CRUD operations on a model.
+ */
+public interface Repository {
+
+    boolean insert(SampleModel model);
+
+    boolean update(SampleModel model);
+
+    SampleModel get(Object id);
+
+    boolean delete(SampleModel model);
+}
